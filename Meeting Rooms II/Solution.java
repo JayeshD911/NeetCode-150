5f9ec20d3f2fa918a0a3f9ffa36e10c20186ef44
Jayesh <jayeshdongre31@gmail.com>
2026-03-26 23:59:51 +0530

    Solved Meeting Rooms II

--- Meeting Rooms II/Solution.java
+++ Meeting Rooms II/Solution.java
@@ -28,3 +28,40 @@ class Solution {
         return minHeap.size();
     }
 }
+
+
+//TWO POINTER APPROACH
+
+class Solution {
+    public int minMeetingRooms(List<Interval> intervals) {
+        int n = intervals.size();
+        int ans = 0;
+        int[] start = new int[n];
+        int[] end = new int[n];
+
+        for (int i = 0; i< n ;i++){
+            start[i] = intervals.get(i).start;
+            end[i] = intervals.get(i).end;
+        }
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int start_position = 0;
+        int end_position = 0;
+        int count = 0;
+        while(start_position < n && end_position < n){
+            if(start[start_position] < end[end_position]){
+                count++;
+                ans = Math.max(ans, count);
+                start_position++;
+            }
+            else{
+                count--;
+                end_position++;
+            }
+        }
+
+        return ans;
+
+    }
+}
