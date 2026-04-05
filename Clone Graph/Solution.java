448c572e80a49480d7823fc6a24286a742e74206
Jayesh <jayeshdongre31@gmail.com>
2026-04-06 00:07:38 +0530

    Solved Clone Graph

--- Clone Graph/Solution.java
+++ Clone Graph/Solution.java
@@ -1,7 +1,11 @@
+//Separate dfs solution to reduce extra computation
 class Solution {
     Map<Node , Node > hm = new HashMap<>();
     public Node cloneGraph(Node node) {
         if (node == null) return null;
+        return dfs(node);
+    }
+    public Node dfs(Node node){
         if (hm.containsKey(node)) return hm.get(node);
 
         Node clone = new Node(node.val);
@@ -13,3 +17,20 @@ class Solution {
         return clone;
     }
 }
+
+// Together
+//class Solution {
+//    Map<Node , Node > hm = new HashMap<>();
+//    public Node cloneGraph(Node node) {
+//        if (node == null) return null;
+//        if (hm.containsKey(node)) return hm.get(node);
+//
+//        Node clone = new Node(node.val);
+//        hm.put(node,clone);
+//        for (Node neighbor : node.neighbors){
+//            clone.neighbors.add(cloneGraph(neighbor));
+//        }
+//
+//        return clone;
+//    }
+//}
