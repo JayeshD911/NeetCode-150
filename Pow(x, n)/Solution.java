0ceb2376fe05bf7b71e2a965f97715bd210c07f1
Jayesh <jayeshdongre31@gmail.com>
2026-05-04 23:57:28 +0530

    Started Pow(x, n)

---
+++ Pow(x, n)/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        // long N = n; //to avoid overflow
+        int N = n;
+        if (n < 0){       // x^(-n) = 1 / x^n
+            x = 1/x;
+            N = -N;
+        }
+        return fastPow(x,N);
+    }
+
+    public double fastPow(double x, int n) {
+        if (n == 0) return 1L;
+
+        double temp = fastPow(x, n/2);
+
+        if( n % 2 == 0) return temp * temp;
+        else return temp * temp * x;
+    }
+}
